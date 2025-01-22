9d92809ed7e7511f3eae03b3b0281721cc67b57e
Prashant1125 <145115819+Prashant1125@users.noreply.github.com>
2025-01-22 18:29:25 +0530

    completed component

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'screens/account_creation.dart';
 
 void main() {
--- lib/screens/account_creation.dart
+++ lib/screens/account_creation.dart
@@ -24,55 +24,49 @@ class AccountCreation extends StatelessWidget {
 
             // This is The main Component
             Expanded(
-              child: Obx(
-                () => GridView.builder(
-                  shrinkWrap: true,
-                  padding: EdgeInsets.zero,
-                  physics: const ClampingScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      childAspectRatio: 3.99,
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 4),
-                  itemCount: languageButtonsController.isSheAll.value
-                      ? languageButtonsController.listOfObjectives.length
-                      : 8,
-                  itemBuilder: (context, index) {
-                    return index == 7 &&
-                            !languageButtonsController.isSheAll.value
-                        ? ElevatedButton(
-                            onPressed: () {
-                              languageButtonsController.changeTapState();
-                            },
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xFF2F5B6C),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            child: Text(
-                              'See All..',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                              ),
+              child: GridView.builder(
+                shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                physics: const ClampingScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 3.99,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 4),
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return index == 7
+                      ? ElevatedButton(
+                          onPressed: () {
+                            _showDialoge(context, languageButtonsController);
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF2F5B6C),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                          child: Text(
+                            'See All..',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
                             ),
-                          )
-                        : LanguageButton(
-                            onTap: () {
-                              languageButtonsController
-                                  .changeStateForMembershipObjectives(
-                                      languageButtonsController
-                                              .listOfObjectivesButtonUniqueId[
-                                          index],
-                                      index);
-                            },
-                            buttonHeight: 45,
-                            buttonText: languageButtonsController
-                                .listOfObjectivesButtonUniqueId[index],
-                            uniqueButtonId: languageButtonsController
-                                .listOfObjectivesButtonUniqueId[index]);
-                  },
-                ),
+                          ),
+                        )
+                      : LanguageButton(
+                          onTap: () {
+                            languageButtonsController
+                                .changeStateForMembershipObjectives(
+                                    languageButtonsController
+                                        .listOfObjectivesButtonUniqueId[index],
+                                    index);
+                          },
+                          buttonHeight: 45,
+                          buttonText: languageButtonsController
+                              .listOfObjectivesButtonUniqueId[index],
+                          uniqueButtonId: languageButtonsController
+                              .listOfObjectivesButtonUniqueId[index]);
+                },
               ),
             ),
 
@@ -80,5 +74,53 @@ class AccountCreation extends StatelessWidget {
             ElevatedButton(onPressed: () {}, child: Text('My Name is Pk')),
           ],
         ));
+  }
+
+  void _showDialoge(BuildContext context,
+      LanguageButtonsController languageButtonsController) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Scaffold(
+          backgroundColor: Colors.grey,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: SizedBox(
+            width: Get.width * .8,
+            height: 45,
+            child: FloatingActionButton(
+                backgroundColor: Color(0XffB8FE22),
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text('Selected')),
+          ),
+          body: GridView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            physics: const ClampingScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 3.99, crossAxisCount: 2, mainAxisSpacing: 4),
+            itemCount:
+                languageButtonsController.listOfObjectivesButtonUniqueId.length,
+            itemBuilder: (context, index) {
+              return LanguageButton(
+                  onTap: () {
+                    languageButtonsController
+                        .changeStateForMembershipObjectives(
+                            languageButtonsController
+                                .listOfObjectivesButtonUniqueId[index],
+                            index);
+                  },
+                  buttonHeight: 45,
+                  buttonText: languageButtonsController
+                      .listOfObjectivesButtonUniqueId[index],
+                  uniqueButtonId: languageButtonsController
+                      .listOfObjectivesButtonUniqueId[index]);
+            },
+          ),
+        );
+      },
+    );
   }
 }
